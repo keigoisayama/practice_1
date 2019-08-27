1a8d86681134c834c95281b2d03f1f5b94774584
keigoisayama <ou.1651081@gmail.com>
2019-08-27 17:34:52 +0900

    2nd commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var option = {
+    section : '.js-section',
+    easing: "swing",
+    scrollSpeed: 600,
+    scrollbars: true,
+  };
+  
+  $(function() {
+    $.scrollify(option); // scrollifyの実行
+  });
---
+++ scrollify.js
@@ -0,0 +1,19 @@
+$.scrollify({
+    section : ".example-classname",
+    sectionName : "section-name",
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 700,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
+  
